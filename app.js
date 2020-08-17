467587b61a5d1904dadf1e9d9a853dcebfeb9b36
Jasper Cheyney <jasper@cheyneymail.co.uk>
2020-08-17 21:56:47 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle('open');
