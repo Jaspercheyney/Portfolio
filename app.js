23a2d5cfbc000d02782817c3c6aa9e82f3dfff3a
Jasper Cheyney <jasper@cheyneymail.co.uk>
2020-09-16 14:20:37 +0100

    Testing viewport animation

--- app.js
+++ app.js
@@ -9,3 +9,47 @@ hamburger.addEventListener('click', () => {
      hamburger.classList.toggle('open');
   });
 });
+
+// get all the elements
+elements = document.getElementsByClassName('show-on-scroll');
+
+function checkPosition() {
+
+    for (i = 0; i < elements.length; i++) {
+      
+      // get the elements position
+      var position = elements[i].getBoundingClientRect();
+      var positionBottom = position.bottom + 0;
+      var positionTop = position.top + 50;
+
+      // checking whether fully visible
+      if(positionTop >= 0 && positionBottom <= window.innerHeight) {
+        console.log('Element is fully visible in screen');
+        element[i].classList.add('is-visible');
+      }
+
+      // checking for partial visibility
+      if(positionTop < window.innerHeight && positionBottom >= 0) {
+        console.log('Element is partially visible in screen');
+        element[i].classList.add('is-visible');
+      }
+  }
+
+}
+
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function() {
+    let context = this, args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+window.addEventListener('scroll', debounce(checkPosition));
