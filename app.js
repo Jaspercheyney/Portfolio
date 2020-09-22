64f746a54f25d4da25fdeb1ebb7a65769ff8ea76
Jasper Cheyney <jasper@cheyneymail.co.uk>
2020-09-22 16:39:38 +0100

    Dark Mode code test

--- app.js
+++ app.js
@@ -55,3 +55,29 @@ function debounce(func, wait = 10, immediate = true) {
 
 checkPosition()
 window.addEventListener('scroll', debounce(checkPosition));
+
+
+// Dark Mode
+
+// On page load set the theme.
+(function() {
+  let onpageLoad = localStorage.getItem("theme") || "";
+  let element = document.body;
+  element.classList.add(onpageLoad);
+  document.getElementById("theme").textContent =
+    localStorage.getItem("theme") || "light";
+})();
+
+function themeToggle() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+
+  let theme = localStorage.getItem("theme");
+  if (theme && theme === "dark-mode") {
+    localStorage.setItem("theme", "");
+  } else {
+    localStorage.setItem("theme", "dark-mode");
+  }
+
+  document.getElementById("theme").textContent = localStorage.getItem("theme");
+}
