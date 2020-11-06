ae320159b4138c0ac492489d2b17e92ebabed8f5
Jasper Cheyney <jasper@cheyneymail.co.uk>
2020-11-06 17:41:22 +0000

    Fixing mobile menu bug
    (hopefully)

--- app.js
+++ app.js
@@ -86,9 +86,11 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar")
+    .classList.remove('navbar-hide');
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+        document.getElementById("navbar")
+    .classList.add('navbar-hide');
   }
   prevScrollpos = currentScrollPos;
 }
