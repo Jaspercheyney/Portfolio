5eaf073faaf630109ea296ddec7fe4ef8759edd2
Jasper Cheyney <jasper@cheyneymail.co.uk>
2020-11-06 18:03:32 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var handleMenuToggle = function (event) {
 
 // Add our event listeners
 hamburger.addEventListener('click', handleMenuToggle, false);
+hamburger.addEventListener('ontouchstart', handleMenuToggle, false);
 
 // Event listener for nav links
 document.querySelectorAll('.nav-links li a').forEach(item => {
